f48f13b7d619ab7eb9c63d110c9d9fa3eeaa4b53
notcroks <mikhail.sukharev.m@gmail.com>
2022-02-26 20:40:48 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -26,6 +26,20 @@ var swiper = new Swiper(".offerSwiper", {
       prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1000: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      }
+    }
 });
 
 var swiper = new Swiper(".trendsSwiper", {
